f15b699e5b1c8fe7004db6a7e785ac2412df040f
Nevia1020 <79929396+Nevia1020@users.noreply.github.com>
2021-03-12 11:21:43 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,9 @@
+let umpire = 0
+input.onButtonPressed(Button.A, function () {
+    basic.showNumber(randint(3, 4))
+    basic.showString("" + (umpire))
+    umpire = 0
+})
+basic.forever(function () {
+	
+})
